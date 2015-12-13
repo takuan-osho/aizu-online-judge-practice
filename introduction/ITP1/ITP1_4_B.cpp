8fc3236d1b78a8e0f876095726a066303ba20190
SHIMIZU Taku <shimizu.taku@gmail.com>
2015-12-13 21:53:50 +0900

    Solved "Circle"

---
+++ introduction/ITP1/ITP1_4_B.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+  double r;
+  double circumference, area;
+
+  cin >> r;
+
+  circumference = r * r * M_PI;
+  area = 2 * M_PI * r;
+
+  printf("%lf %lf\n", circumference, area);
+}
