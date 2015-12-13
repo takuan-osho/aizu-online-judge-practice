bbb6fabed02ba40814e855316e1045beddd6c142
SHIMIZU Taku <shimizu.taku@gmail.com>
2015-12-13 21:34:30 +0900

    Solved "A / B Problem"

---
+++ introduction/ITP1/ITP1_4_A.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+  int d;
+  int r;
+  double f;
+
+  cin >> a >> b;
+
+  d = a / b;
+  r = a % b;
+  f = (double)a / (double)b;
+  printf("%d %d %f\n", d, r, f);
+}
