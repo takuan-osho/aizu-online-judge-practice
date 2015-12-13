d1c59137486df8d51ed203781c7b4afd0d6a094e
SHIMIZU Taku <shimizu.taku@gmail.com>
2015-12-14 08:34:38 +0900

    Solved "Min, Max and Sum"

---
+++ introduction/ITP1/ITP1_4_D.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  int a;
+  int minValue = 1000000;
+  int maxValue = -1000000;
+  long long sum = 0;
+
+  for (int i = 0; i < n; i++) {
+    cin >> a;
+
+    minValue = (a < minValue) ? a : minValue;
+    maxValue = (a > maxValue) ? a : maxValue;
+
+    sum += a;
+  }
+
+  cout << minValue << " " << maxValue << " " << sum << endl;
+  return 0;
+}
