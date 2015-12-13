d30619dd4cda6c9e60737fdf70b9c62218302834
SHIMIZU Taku <shimizu.taku@gmail.com>
2015-12-13 20:59:28 +0900

    Solved "How Many Divisors?"

---
+++ introduction/ITP1/ITP1_3_D.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b, c;
+  int count = 0;
+  cin >> a >> b >> c;
+
+  for (int x = a; x <= b; x++) {
+    if (c % x == 0) {
+      count++;
+    }
+  }
+  cout << count << endl;
+
+  return 0;
+}
