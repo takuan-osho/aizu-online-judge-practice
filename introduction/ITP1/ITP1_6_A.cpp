08317cd3db0ece1f5748f153fc778d1c6a7572b3
SHIMIZU Taku <shimizu.taku@gmail.com>
2015-12-14 20:32:00 +0900

    Solved "Reversing Numbers"

---
+++ introduction/ITP1/ITP1_6_A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  int digits[n];
+  for (int i = 0; i < n; i++) {
+    cin >> digits[i];
+  }
+
+  for (int i = n-1; i >= 0; i--) {
+    if (i != n-1) {
+      cout << " ";
+    }
+    cout << digits[i];
+  }
+  cout << endl;
+}
