17262e7970a20e5e54bb093908595329e4c0ba24
SHIMIZU Taku <shimizu.taku@gmail.com>
2015-12-14 10:44:01 +0900

    Solved "Structured Programming"

---
+++ introduction/ITP1/ITP1_5_D.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool hasThree(int x) {
+  while (x) {
+    if (x % 10 == 3) {
+      return true;
+    }
+    x /= 10;
+  }
+  return false;
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  for (int i = 1; i <= n; i++) {
+    if (i % 3 == 0 || hasThree(i)) {
+      cout << " " << i;
+    }
+  }
+
+  cout << endl;
+  return 0;
+}
