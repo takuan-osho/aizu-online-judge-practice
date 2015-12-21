de171f08d50c3e5aeb32c898758f40c095adb0fc
SHIMIZU Taku <shimizu.taku@gmail.com>
2015-12-21 12:37:18 +0900

    更に追加

---
+++ introduction/ITP1/ITP1_6_D.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+
+  // 初期化
+  int A[100][100];
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> A[i][j];
+    }
+  }
+  int b[100];
+  for (int i = 0; i < m; i++) {
+    cin >> b[i];
+  }
+
+  int c[100];
+  for (int i = 0; i < n; i++) {
+    c[i] = 0;
+    for (int j = 0; j < m; j++) {
+      c[i] += A[i][j] * b[j];
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    cout << c[i] << endl;
+  }
+}
